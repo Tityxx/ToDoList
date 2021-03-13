d1b5bd5c6d2ed53fb0d230a32c1a8197af2a5b05
Tityxx <nikitka_nik1@mail.ru>
2021-03-13 18:48:13 +0700

    Вёрстка окна авторизации/регистрации, добавление новых спрайтов

---
+++ Assets/GameResources/Scripts/UI/TextColorController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Смена цвета текста на тоггле
+/// </summary>
+public class TextColorController : MonoBehaviour
+{
+    [SerializeField]
+    private Text text;
+
+    private Toggle toggle;
+
+    private void Awake()
+    {
+        toggle = GetComponent<Toggle>();
+        toggle.onValueChanged.AddListener(ChangeColor);
+    }
+
+    private void OnDestroy()
+    {
+        toggle.onValueChanged.RemoveListener(ChangeColor);
+    }
+
+    public void ChangeColor(bool isOn)
+    {
+        Color tmpColor = text.color;
+        tmpColor.a = isOn ? 1 : 0.5f;
+        text.color = tmpColor;
+    }
+}
