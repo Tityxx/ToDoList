6f1f42679013cfde928c8c2bec9adf0394b31e3d
Tityxx <nikitka_nik1@mail.ru>
2021-03-13 16:33:58 +0700

    Залил спрайты

---
+++ Assets/GameResources/Scripts/ScriptableObjects/TasksContainer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+/// <summary>
+/// SO для хранения тасок и id юзера,
+/// под которым авторизовались
+/// </summary>
+[CreateAssetMenu]
+public class TasksContainer : ScriptableObject
+{
+    public string userId;
+    public List<Task> Tasks;
+
+    
+
+}
